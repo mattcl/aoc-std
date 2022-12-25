f1bb67745b7ae6a4e77d335857b84f8b9b24535e
Matt Chun-Lum <matt@questionable.engineering>
2022-12-25 12:40:11 -0800

    feat: add conversions

---
+++ aoc-conversions/src/chars.rs
@@ -0,0 +1,92 @@
+/// Converts an ascii alpha char to u8.
+///
+/// The conversion is as follows:
+///
+///   `a..=z -> 0..=25`
+///
+///   `A..=Z -> 26..=51`
+///
+/// This is unchecked and does not verify the char is ascii. That should be
+/// checked elsewhere.
+#[inline]
+pub fn ascii_alpha_to_num(ch: char) -> u8 {
+    if ch.is_lowercase() {
+        (ch as u8) - ('a' as u8)
+    } else {
+        (ch as u8) - ('A' as u8) + 26
+    }
+}
+
+/// Converts a lowercase ascii alpha char to u8.
+///
+/// The conversion is as follows:
+///
+///   `a..=z -> 0..=25`
+///
+/// This is unchecked and does not verify the char is ascii or lowercase. That
+/// should be checked elsewhere.
+#[inline]
+pub fn ascii_lowercase_alpha_to_num(ch: char) -> u8 {
+    (ch as u8) - ('a' as u8)
+}
+
+/// Converts a u8 produced by [ascii_alpha_to_num] back to a char.
+///
+/// The conversion is as follows:
+///
+///   `0..=25 -> a..=z`
+///
+///   `26..=51 -> A..=Z`
+///
+/// This is unchecked and does not verify the value is valid. That should be
+/// checked elsewhere.
+#[inline]
+pub fn num_to_ascii_alpha(num: u8) -> char {
+    if num > 25 {
+        (num + ('A' as u8) - 26) as char
+    } else {
+        (num + ('a' as u8)) as char
+    }
+}
+
+/// Converts a u8 produced by [ascii_lowercase_alpha_to_num] back to a char.
+///
+/// The conversion is as follows:
+///
+///   `0..=25 -> a..=z`
+///
+/// This is unchecked and does not verify the value is valid. That should be
+/// checked elsewhere.
+#[inline]
+pub fn num_to_lowercase_ascii_alpha(num: u8) -> char {
+    (num + ('a' as u8)) as char
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn to_num() {
+        for (idx, ch) in ('a'..'z').enumerate() {
+            assert_eq!(ascii_alpha_to_num(ch), idx as u8);
+            assert_eq!(ascii_lowercase_alpha_to_num(ch), idx as u8);
+        }
+
+        for (idx, ch) in ('A'..'Z').enumerate() {
+            assert_eq!(ascii_alpha_to_num(ch), idx as u8 + 26);
+        }
+    }
+
+    #[test]
+    fn to_char() {
+        for (idx, ch) in ('a'..'z').enumerate() {
+            assert_eq!(num_to_ascii_alpha(idx as u8), ch);
+            assert_eq!(num_to_lowercase_ascii_alpha(idx as u8), ch);
+        }
+
+        for (idx, ch) in ('A'..'Z').enumerate() {
+            assert_eq!(num_to_ascii_alpha(idx as u8 + 26), ch);
+        }
+    }
+}
--- aoc-std/src/lib.rs
+++ aoc-std/src/lib.rs
@@ -1,3 +1,4 @@
 // re-exports at the top level
+pub use aoc_conversions as conversions;
 pub use aoc_directions as directions;
 pub use aoc_geometry as geometry;
