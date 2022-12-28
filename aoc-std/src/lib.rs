3ab080d1a8144318a2303ece4cc3f175294e7a6a
Matt Chun-Lum <matt@questionable.engineering>
2022-12-27 16:31:36 -0800

    feat: expose collecitons/pathing

--- aoc-std/src/lib.rs
+++ aoc-std/src/lib.rs
@@ -1,4 +1,6 @@
 // re-exports at the top level
+pub use aoc_collections as collections;
 pub use aoc_conversions as conversions;
 pub use aoc_directions as directions;
 pub use aoc_geometry as geometry;
+pub use aoc_pathing as pathing;
