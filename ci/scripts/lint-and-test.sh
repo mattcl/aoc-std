574223362d178d4ef56d2493f4436ed6ee9e246c
Matt Chun-Lum <matt@questionable.engineering>
2023-11-12 15:37:55 -0800

    ci: custom lint and test

---
+++ ci/scripts/lint-and-test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ex
+
+if [ "$LINT" -eq 1 ]; then
+    # make sure we're formatted
+    cargo fmt --check
+
+    # fail on clippy warnings
+    cargo clippy -- -Dwarnings
+fi
+
+# ensure we can build
+cargo build --verbose ${EXTRA_CARGO_BUILD_FLAGS}
+
+# ensure tests pass
+cargo test --verbose ${EXTRA_CARGO_TEST_FLAGS}
+
+# benchmark
+cargo bench -q -p benchmarking
