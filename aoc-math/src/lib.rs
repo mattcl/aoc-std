6bee9749a6345933c9f9ec77c04f96b857273688
Matt Chun-Lum <matt@questionable.engineering>
2024-12-14 13:23:58 -0800

    fix(math): mad mod inv on self

--- aoc-math/src/lib.rs
+++ aoc-math/src/lib.rs
@@ -1,19 +1,19 @@
 pub trait ModInv {
-    fn mod_inv(a: Self, m: Self) -> Self;
+    fn mod_inv(&self, m: Self) -> Self;
 }
 
 macro_rules! impl_integers {
     ($($x:ty),+ $(,)?) => {
         $(
             impl ModInv for $x {
-                fn mod_inv(a0: Self, m0: Self) -> Self {
+                fn mod_inv(&self, m0: Self) -> Self {
                     if m0 == 1 { return 1 }
 
-                    let (mut a, mut m, mut x0, mut inv) = (a0, m0, 0, 1);
+                    let (mut a, mut m, mut x0, mut inv) = (*self, m0, 0, 1);
 
                     while a > 1 {
                         inv -= (a / m) * x0;
-                        a = a % m;
+                        a %= m;
                         std::mem::swap(&mut a, &mut m);
                         std::mem::swap(&mut x0, &mut inv);
                     }
